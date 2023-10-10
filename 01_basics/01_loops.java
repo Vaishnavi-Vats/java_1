4f81272c077e1205a4f8f6844153ccb910bbdafc
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-10-10 17:57:07 +0000

    practice

--- 01_basics/01_loops.java
+++ 01_basics/01_loops.java
@@ -1,5 +1,21 @@
-int sum=0;
-for(int i=0;i<n;i++){
-    sum=sum+i;
+pakage Fundamental{
+    public class LoopPractice{
+        public static void main(string args[]){
+            int sum=0;
+            for (int i=0;i<4; i++){
+                sum = sum+i;
+            }
+            System.out.println(sum);
+            
+
+        }
+    }
 }
-System.out.println(sum);
+
+
+
+// int sum=0;
+// for(int i=0;i<n;i++){
+//     sum=sum+i;
+// }
+// System.out.println(sum);
