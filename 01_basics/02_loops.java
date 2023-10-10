8254dfab31f35fccade63a622c3e6d9dd786ed61
Vaishnavi Singh <138869431+Vaishnavi-Vats@users.noreply.github.com>
2023-10-10 18:08:57 +0000

    practice

---
+++ 01_basics/02_loops.java
@@ -0,0 +1,12 @@
+package Fundamental;
+import java.util.*;
+public class LoopPractice{
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=0;i<9;i++){
+            sum=sum+i;
+
+        }
+        System.out.println(sum);
+    }
+}
